b7b0ebf8f089c2cc6ab91da72d7d65f0a05375d2
Pranay Aryal <drpranayaryal@gmail.com>
2018-05-28 23:43:48 -0400

    made changes final to server

--- server/models/user.js
+++ server/models/user.js
@@ -9,19 +9,24 @@ const userSchema = new Schema({
 })
 
 //On save hook, encrypt password
-userSchema.pre('save', (next) => {
+userSchema.pre('save', function(next) {
+    // get access to the user model
     const user = this;
-    bcrypt.genSalt(10, (err, salt) => {
+  
+    // generate a salt then run callback
+    bcrypt.genSalt(10, function(err, salt) {
+      if (err) { return next(err); }
+  
+      // hash (encrypt) our password using the salt
+      bcrypt.hash(user.password, salt, null, function(err, hash) {
         if (err) { return next(err); }
-
-        bcrypt.hash(user.password, salt, null, (err, hash) => {
-            if (err) { return next(err); }
-
-            user.password = hash;
-            next();
-        })
-    })
-})
+  
+        // overwrite plain text password with encrypted password
+        user.password = hash;
+        next();
+      });
+    });
+  });
 
 userSchema.methods.comparePasswords = function(candidatePassword, callback){
     bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
